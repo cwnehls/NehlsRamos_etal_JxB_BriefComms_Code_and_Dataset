8d1e717384e35ede4df3ed0f855a34e6ac672e47
cwnehls <westcon888@gmail.com>
2026-01-16 17:48:10 +0000

    Update S Figure 1a Methods Comparison by Species.R

--- S Figure 1a Methods Comparison by Species.R
+++ S Figure 1a Methods Comparison by Species.R
@@ -2,7 +2,7 @@
 
 
 # Method Comparisons by Species
-# Date of last edit: 12/11/2025
+# Date of last edit: 16/01/2026
 # Description: This code is meant to be ran altogether at once. For plotting differences in Rubisco dark 
   # dark inhibition across species that have data from all three methods used in literature for 
   # identifying dark inhibition values. 
@@ -50,16 +50,17 @@ Species_Labels <- c("Alocasia macrorrhiza", "Triticum aestivum", "Megathyrsus ma
 ggplot(data = subset(df, !is.na(Species)), aes(x = Species, y = Dark_Inhibition)) +
   geom_hline(yintercept = c(0.18, 0.44, 0.77), 
              linetype = "dashed", color = "grey70") +
-  geom_jitter(width = 0.15, height = 0, alpha = 0.6, size = 2, aes(x = Species, y = Dark_Inhibition, color = Method)) +  # Show individual points
-
+  geom_jitter(width = 0.15, height = 0, alpha = 0.6, size = 3, aes(x = Species, y = Dark_Inhibition, color = Method)) +  # Show individual points
   scale_color_viridis_d(option = "viridis") +
   theme_minimal() +
-  theme(axis.text.y = element_text(size = 12), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 12, face = "italic"), 
-        axis.title = element_text(size = 16, face = "bold"), legend.position = "bottom", legend.text = element_text(size = 12),
-        legend.title = element_text(size = 12, face = "bold"), plot.title = element_text(size = 16.5, face = "bold")) +
+  theme(axis.text.y = element_text(size = 14), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 14, face = "italic"), 
+        axis.title = element_text(size = 18, face = "bold"), legend.position = "bottom", legend.text = element_text(size = 14),
+        legend.title = element_text(size = 14, face = "bold"), plot.title = element_text(size = 18, face = "bold")) +
   scale_x_discrete(labels = Species_Labels) +
+  scale_y_continuous(labels = scales::percent) +
   labs(x = "Species", y = "Dark Inhibition") +
   guides(colour = guide_legend(override.aes = list(size=4), title = "Method: "))
 
 
 # ---- End ----
+
